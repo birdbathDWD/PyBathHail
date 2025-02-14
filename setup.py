b6db6e58c255969585a416aaa63d56d40b791079
birdbathDWD <mathias.gergely@dwd.de>
2025-02-14 13:11:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='PyBathHail',
-    version='0.1.0',
+    version='0.1.1',
     description='Retrieval of hail size distributions from DWD birdbath scan',
     long_description=long_description,
     # Content types: text/plain, text/x-rst, and text/markdown
